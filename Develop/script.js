1fd69369bec4602ac2edabb6fee94afa51dd7e82
Robby Hill <robbyhill20@yahoo.com>
2021-09-29 21:47:32 -0700

    updating

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase= ["a","b","c","d","e","f"]
-var uppercase= ["A", "B","C","D","E","F"]
-var numbers= [1,2,3,4,5,6,7,8]
-var special=["#","@","%","&","$"]
+var lowercase= ["abcdefghi"]
+var uppercase= ["ABCDEFGHIJK"]
+var numbers= ["12345678"]
+var special=["#@&!$"]
 var characters=[0]
 var wantLower= true
 var wantUpper= true
@@ -15,25 +15,20 @@ var values = [""]
 // Write password to the #password input
 var generatePassword= function(){
 var pwLength= (prompt("How long would you like your password to be? Please choose a number beetween 8 and 128"))
-  console.log(pwLength)
   if(pwLength < 8){
     return alert("Please choose a higher number")
   }
     else if(pwLength > 128) {
       return alert("Please choose a lower number")
     }
-  
   var generateUpper=confirm("Do you want uppercase letters in your password?")
   if(!generateUpper) {
     wantUpper=false
    } else(++characters)
-  console.log(generateUpper)
   var generateLower=confirm("Do you want lowercase letters in your password?")
   if(!generateLower) {
     wantLower=false
   }   else(++characters)
-  console.log(generateLower)
-  console.log(characters)
   if(characters < 1){
     return alert ("You must choose an upper or lowercase letter for your password")
   }
@@ -42,8 +37,6 @@ var pwLength= (prompt("How long would you like your password to be? Please choos
     wantNumbers=false
     }
     else(++characters)
-    
-   
     var generateSpecial=confirm("Do you want special characters in your password?")
     if(!generateSpecial) {
       wantSpecial=false
@@ -53,12 +46,6 @@ var pwLength= (prompt("How long would you like your password to be? Please choos
       if(characters < 2){
         return alert("Your password must contain more than one element ")
       }
-      
-      console.log(wantUpper)
-      console.log(wantLower)
-      console.log(wantNumbers)
-      console.log(wantSpecial)
-      console.log(characters)
     //adding all the chosen items to the values array
     if(characters==4){
       values= uppercase + lowercase + numbers + special 
@@ -81,7 +68,6 @@ var pwLength= (prompt("How long would you like your password to be? Please choos
       else if(wantUpper==true && wantSpecial==true){
         values =uppercase + special
       }
-
       else if(wantLower==true && wantNumbers==true && wantSpecial ==true){
         values =special +lowercase +numbers
       } 
@@ -91,19 +77,12 @@ var pwLength= (prompt("How long would you like your password to be? Please choos
       else if(wantLower==true && numbers==true){
         values =numbers +lowercase
       }
-
-  
-      var removeCommas= values.replace(/,/g,'')
-    
-    
-     //for loop to create password
+      
+    //for loop to create password
     for (var i =0; i <= pwLength; i++){
-      var random= Math.floor(Math.random()*removeCommas.length);
-      password += removeCommas.substring(random, random +1);
-     
+      var random= Math.floor(Math.random()*values.length);
+      password += values.substring(random, random +1); 
   }  
-  console.log(password) 
-  
   document.getElementById("password").value = password
 }
 generateBtn.addEventListener("click",generatePassword)
